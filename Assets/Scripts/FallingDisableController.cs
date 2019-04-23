0777a815a203f11b3ecccf586c4ba70b3a5a67da
Muyuan Li <li2430@purdue.edu>
2019-04-22 20:18:07 -0400

    Second Scene
    Second Scene Added

---
+++ Assets/Scripts/FallingDisableController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingDisableController : MonoBehaviour
+{
+    private PlayerManager playerManager;
+    private GameObject playerObject;
+
+    public ClimbingTriggerNoGrav climbTrigger;
+    public GameObject baseLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerManager = PlayerManager.instance;
+        playerObject = playerManager.gameObject;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        triggered(other);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        triggered(other);
+    }
+
+    private void triggered(Collider other)
+    {
+        //Debug.Log(other.name);
+        if (other.gameObject.Equals(playerObject))
+        {
+            climbTrigger.endFall();
+            //Debug.Log(gameObject.name);
+            playerObject.transform.position = new Vector3(playerObject.transform.position.x, baseLevel.transform.position.y, playerObject.transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/TorchController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchController : MonoBehaviour
+{
+    public GameObject fire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        MatchController mc = other.gameObject.GetComponent<MatchController>();
+        if(mc != null)
+        {
+            if (mc.isOnFire())
+            {
+                fire.SetActive(true);
+            }
+        }
+    }
+}
